c1003ad2ed35ce3ea27a14ff5d2cd80d55f067cd
David Lee <lihongyi0512@gmail.com>
2023-03-09 04:45:19 +0800

    “dfs”

---
+++ 8-DFS/main.py
@@ -0,0 +1,45 @@
+"""
+112. Path Sum
+
+Given the root of a binary tree and an integer targetSum, 
+return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+A leaf is a node with no children.
+
+https://leetcode.com/problems/path-sum/description/
+
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        if root is None:
+            return False
+        if root.val == sum and root.left is None and root.right is None:
+            return True
+
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+
+## solution2 
+
+def hasPathSum(self, root, sum):
+        if not root:
+            return False
+
+        if not root.left and not root.right and root.val == sum:
+            return True
+        
+        sum -= root.val
+
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)  
+################################################################
+################################################################
+################################################################

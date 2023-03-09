8edabeb8868d81aa6717b3d9b91728cf6eaeb21b
David Lee <lihongyi0512@gmail.com>
2023-03-09 15:29:23 +0800

    dfs

--- 8-DFS/main.py
+++ 8-DFS/main.py
@@ -69,3 +69,34 @@ Output: []
 https://leetcode.com/problems/path-sum-ii/
 
 """
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.dfs(root, [], sum, result)
+        return result
+
+    def dfs(self, root, path, sum, result):
+        if not root:
+            return
+        path.append(root.val)
+        if root.val == sum and not root.left and not root.right:
+            result.append(list(path))
+        else: 
+            self.dfs(root.left, path, sum - root.val, result) 
+            self.dfs(root.right, path, sum - root.val, result)
+        path.pop()
+
+################################################################
+################################################################
+################################################################

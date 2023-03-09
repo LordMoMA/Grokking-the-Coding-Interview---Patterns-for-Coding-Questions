df70c28ab481c7590172b8e04fe80961cc2fda23
David Lee <lihongyi0512@gmail.com>
2023-03-09 15:00:42 +0800

    dfs

--- 8-DFS/main.py
+++ 8-DFS/main.py
@@ -47,3 +47,25 @@ def hasPathSum(self, root, sum):
 ################################################################
 ################################################################
 ################################################################
+
+"""
+113. Path Sum II
+
+
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. 
+Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+
+https://leetcode.com/problems/path-sum-ii/
+
+"""

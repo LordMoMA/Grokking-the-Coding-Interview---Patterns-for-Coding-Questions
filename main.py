2f10cd08cac8a0a348c2478e991e994c8f9c6745
David Lee <lihongyi0512@gmail.com>
2023-03-09 16:03:28 +0800

    dfs

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-path = [1, 2, [3, 4]]
-path_copy = path[:]
-path[2].append(5)
+path = [1, 2, 3]
+path_copy1 = path[:]
+path_copy2 = list(path)
 
-print(path)       # Output: [1, 2, [3, 4, 5]]
-print(path_copy)  # Output: [1, 2, [3, 4]]
+path.append(4)
+
+print(path)        # Output: [1, 2, 3, 4]
+print(path_copy1)  # Output: [1, 2, 3]
+print(path_copy2)  # Output: [1, 2, 3]
+

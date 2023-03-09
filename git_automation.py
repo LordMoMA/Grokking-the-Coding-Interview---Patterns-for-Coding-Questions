a169f4c239f9a5bc0fc8543729e0005d4b0ed582
David Lee <lihongyi0512@gmail.com>
2023-03-09 14:46:29 +0800

    dfs

--- git_automation.py
+++ git_automation.py
@@ -5,4 +5,4 @@ while True:
     os.system('git add .')
     os.system('git commit -m "dfs"')
     os.system('git push -u origin main')
-    time.sleep(120) # sleep for 30 seconds
+    time.sleep(300) # sleep for 5 min

f5d0b1e32bc0ad8d916162db67542e841a75d62c
David Lee <lihongyi0512@gmail.com>
2023-03-09 22:34:28 +0800

    dfs

--- 8-DFS/main.py
+++ 8-DFS/main.py
@@ -112,4 +112,35 @@ class Solution(object):
 Problem 1: Given a binary tree, return all root-to-leaf paths.
 
 Solution: We can follow a similar approach. We just need to remove the “check for the path sum”.
+
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
 """
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        result = []
+        self.dfs(root, "", result)
+        return result
+        
+    
+    def dfs(self, root, path, result):
+        if not root:
+            return None
+        if not root.left and not root.right:
+            result.append(path + str(root.val))
+        if root.left:
+            self.dfs(root.left, path + str(root.val) + "->", result)
+        if root.right:
+            self.dfs(root.right, path + str(root.val) + "->", result)
+        

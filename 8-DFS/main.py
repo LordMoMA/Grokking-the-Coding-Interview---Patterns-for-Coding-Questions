f53dbf975e016810e26b8b7cd887b7d31e4ecd40
David Lee <lihongyi0512@gmail.com>
2023-03-19 01:13:51 +0800

    dfs

--- 8-DFS/main.py
+++ 8-DFS/main.py
@@ -206,4 +206,44 @@ class Solution:
         
         dfs(root)
         return res
-# time O(n) space O(height of the tree) or O(h) or O(logn) if it's a balanced binary tree
+# time O(n) space O(height of the tree) or O(h) or O(logn) if it's a balanced binary 
+
+'''
+129. Sum Root to Leaf Numbers
+https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        return self.dfs(root, 0)
+
+    def dfs(self, node, path_sum):
+        if not node:
+            return 0
+        path_sum = 10 * path_sum + node.val
+
+        if not node.left and not node.right:
+            return path_sum
+        return self.dfs(node.left, path_sum) + self.dfs(node.right, path_sum)
+# Time, Space: O(n)

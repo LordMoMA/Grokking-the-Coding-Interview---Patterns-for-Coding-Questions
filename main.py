9ce688f93817c37f7e8fbda878e10ec9e60c195e
David Lee <lihongyi0512@gmail.com>
2023-03-19 00:58:39 +0800

    dfs

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-path = [1, 2, 3]
-path_copy1 = path[:]
-path_copy2 = list(path)
-
-path.append(4)
-
-print(path)        # Output: [1, 2, 3, 4]
-print(path_copy1)  # Output: [1, 2, 3]
-print(path_copy2)  # Output: [1, 2, 3]
+cur = ""
+a = 1
+print(cur + str(a))
 

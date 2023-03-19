494c68f52c80e59da287aceb8a1c80cfeff8948a
David Lee <lihongyi0512@gmail.com>
2023-03-20 02:08:09 +0800

    dfs

--- 8-DFS/main.py
+++ 8-DFS/main.py
@@ -344,3 +344,24 @@ Input: root = [1,2,3,4,5]
 Output: 3
 Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
 '''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.count = 0
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.dfs(root)
+        return self.count
+       
+    def dfs(self, root):
+        if not root:
+            return 0
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        self.count = max(self.count, left + right + 1)
+        return max(left, right) + 1

7ffbd6b406ce110117d20a0b3da82a831c83b914
David Lee <lihongyi0512@gmail.com>
2023-03-19 23:31:05 +0800

    dfs

--- 8-DFS/main.py
+++ 8-DFS/main.py
@@ -290,3 +290,17 @@ def main():
 
 main()
 # Time, Space O(n) The worst case will happen when the given tree is a linked list (i.e., every node has only one child).
+
+'''
+437. Path Sum III
+
+https://leetcode.com/problems/path-sum-iii/description/
+
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+Output: 3
+Explanation: The paths that sum to 8 are shown.
+'''

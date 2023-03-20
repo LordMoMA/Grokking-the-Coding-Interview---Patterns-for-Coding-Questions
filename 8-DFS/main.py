2f1505c23ec57bf4038b1fd3ba8342889843dec3
David Lee <lihongyi0512@gmail.com>
2023-03-20 15:22:47 +0800

    dfs

--- 8-DFS/main.py
+++ 8-DFS/main.py
@@ -384,3 +384,23 @@ Input: root = [1]
 Output: ["1"]
 
 '''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        return self.dfs(root, '')
+
+    
+    def dfs(self, root, path):
+        if not root:
+            return []
+        path += str(root.val)
+        if not root.left and not root.right:
+            return [path]
+        path += '->'
+        return self.dfs(root.left, path) + self.dfs(root.right, path)

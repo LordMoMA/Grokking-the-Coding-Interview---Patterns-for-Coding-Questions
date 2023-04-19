b0e815aba013eabfad26d9fa905ec97673014d7a
David Lee <lihongyi0512@gmail.com>
2023-04-19 22:50:56 +0800

    bfs

--- 7-BFS/main.py
+++ 7-BFS/main.py
@@ -49,3 +49,44 @@ Given the root of a binary tree, return the bottom-up level order traversal of i
 Input: root = [3,9,20,null,null,15,7]
 Output: [[15,7],[9,20],[3]]
 '''
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = deque()
+        if not root:
+            return res
+        queue = deque()
+        queue.append(root)
+        while queue:
+            curLevel = []
+            levelSize = len(queue)
+            for _ in range(levelSize):
+                cur = queue.popleft()
+                curLevel.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            res.appendleft(curLevel)
+        return res
+# time, space O(n)
+
+
+'''
+103. Binary Tree Zigzag Level Order Traversal
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+'''

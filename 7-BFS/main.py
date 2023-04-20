7b9e48a4aa96c6832b53bc2f21bccbb03391af4a
David Lee <lihongyi0512@gmail.com>
2023-04-20 17:57:00 +0800

    bfs

--- 7-BFS/main.py
+++ 7-BFS/main.py
@@ -126,3 +126,75 @@ class Solution:
             leftToRight = not leftToRight
         return res
 # time, space O(n)
+
+
+'''
+637. Average of Levels in Binary Tree
+
+https://leetcode.com/problems/average-of-levels-in-binary-tree/
+Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [3.00000,14.50000,11.00000]
+Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+Hence return [3, 14.5, 11].
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        res = []
+        if not root:
+            return res
+
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            curLevel = []
+            levelSize = len(queue)
+            for _ in range(levelSize):
+                cur = queue.popleft()
+                curLevel.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            levelAverage = sum(curLevel) / levelSize
+            res.append(levelAverage)
+        return res
+
+# solution 2
+
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        res = []
+        if not root:
+            return res
+
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            curLevel = []
+            levelSize = len(queue)
+            levelSum = 0
+            for _ in range(levelSize):
+                cur = queue.popleft()
+                curLevel.append(cur.val)
+                levelSum += cur.val
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            levelAverage = levelSum / levelSize
+            res.append(levelAverage)
+        return res
+# time, space O(n)

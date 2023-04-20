cda31efd19b9cf455aa67ca590a155c9b6d7a7dc
David Lee <lihongyi0512@gmail.com>
2023-04-21 01:52:53 +0800

    bfs

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 def firstMissingPositive(nums):
+    nums.append(0)
     n = len(nums)
 
-    # Step 1: Replace negative and zero elements with n+1
-    for i in range(n):
-        if nums[i] <= 0:
-            nums[i] = n+1
+    for i in range(len(nums)):  # delete those useless elements
+        if nums[i] < 0 or nums[i] >= n:
+            nums[i] = 0
 
-    # Step 2: Mark the presence of positive integers
-    for i in range(n):
-        idx = abs(nums[i])
-        if idx <= n:
-            nums[idx-1] = -abs(nums[idx-1])
+    for i in range(len(nums)):  # use the index as the hash to record the frequency of each number
+        nums[nums[i] % n] += n
+    print(nums)
 
-    # Step 3: Find the first missing positive integer
-    for i in range(n):
-        if nums[i] > 0:
-            return i+1
-
-    return n+1
+    for i in range(1, len(nums)):
+        if nums[i]/n == 0:
+            return i
+    return n
 
 
-nums1 = [1, 2, 0]
-nums2 = [7, 8, 9, 11, 12]
-print(firstMissingPositive(nums1))
+# nums1 = [1, 2, 0]
+nums2 = [3, 4, -1, 1]
+# print(firstMissingPositive(nums1))
 print(firstMissingPositive(nums2))

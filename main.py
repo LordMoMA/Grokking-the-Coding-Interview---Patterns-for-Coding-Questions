137bd667105cbe010a9c22ddff0702c4e3a3d2bb
David Lee <lihongyi0512@gmail.com>
2023-04-30 22:21:47 +0800

    bfs

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print(["Python", "Javascript"][2 > 1]
-      )
+def remove_even(numbers):
+    for number in numbers:
+        if number % 2 != 0:
+            yield number
+
+
+numbers = [1, 2, 7, 4, 8, 11]
+numbers = list(remove_even(numbers))
+print(numbers)

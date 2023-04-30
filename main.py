a6a79057be408f84eb43d8724ccb7e528e19653a
David Lee <lihongyi0512@gmail.com>
2023-04-30 23:52:38 +0800

    bfs

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
-# import decimal
-
-
-# class Account:
-#     def __init__(self, username, balance):
-#         self.username = username
-#         self.balance = balance
-
-#     @classmethod
-#     def from_string(cls, s):
-#         try:
-#             username, balance = s.split()
-#             balance = decimal.Decimal(float(balance))
-#         except ValueError:
-#             return NullAccount()
-
-#         if balance < 0:
-#             return NullAccount()
-#         return cls(username=username, balance=balance)
-
-
-# class NullAccount:
-#     username = ''
-#     balance = 0
-
-#     @classmethod
-#     def from_string(cls, s):
-#         raise NotImplementedError
-
-
-# accounts_data = [
-#     'david 98.5',
-#     'cotton 21',
-#     'invalid_data',
-#     'roland $invalid_balance',
-#     'alfred -3',
-# ]
-
-
-# def caculate_total_balance(accounts_data):
-#     return sum(Account.from_string(s).balance for s in accounts_data)
-
-
-# print(caculate_total_balance(accounts_data))
-
 import decimal
+
+
+class Account:
+    def __init__(self, username, balance):
+        self.username = username
+        self.balance = balance
+
+    @classmethod
+    def from_string(cls, s):
+        try:
+            username, balance = s.split()
+            balance = decimal.Decimal(float(balance))
+        except ValueError:
+            return NullAccount()
+
+        if balance < 0:
+            return NullAccount()
+        return cls(username=username, balance=balance)
+
+
+class NullAccount:
+    username = ''
+    balance = 0
+
+    @classmethod
+    def from_string(cls, s):
+        raise NotImplementedError
+
+
+accounts_data = [
+    'david 98.5',
+    'helen 21',
+    'invalid_data',
+    'steve $invalid_balance',
+    'elon -3',
+]
+
+
+def caculate_total_balance(accounts_data):
+    return sum(Account.from_string(s).balance for s in accounts_data)
+
+
+print(caculate_total_balance(accounts_data))
 
 
 class CreateAccountError(Exception):
@@ -84,10 +82,10 @@ def caculate_total_balance(accounts_data):
 
 accounts_data = [
     'david 98.5',
-    'cotton 21',
+    'helen 21',
     'invalid_data',
-    'roland $invalid_balance',
-    'alfred -3',
+    'steve $invalid_balance',
+    'elon -3',
 ]
 
-print(caculate_total_balance(accounts_data))
+print(caculate_total_balance(accounts_data))  # 119.5

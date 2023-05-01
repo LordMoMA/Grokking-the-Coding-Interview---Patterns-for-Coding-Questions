f88bf676797a569870db30c207fa508079a14f89
David Lee <lihongyi0512@gmail.com>
2023-05-01 23:36:14 +0800

    bfs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-fruits = ['apple', 'banana', 'cherry']
-
-index = 0
-for fruit in fruits:
-    print(f"The fruit at index {index} is {fruit}")
-    index += 1
+# find if there are three numbers in three lists that add up to 12
+def find_twelve(num_list1, num_list2, num_list3):
+    for num1 in num_list1:
+        for num2 in num_list2:
+            for num3 in num_list3:
+                if num1 + num2 + num3 == 12:
+                    return num1, num2, num3

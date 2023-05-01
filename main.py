ef607487bd5e141e67e3f6c2a90bed6dbcb3c053
David Lee <lihongyi0512@gmail.com>
2023-05-02 00:21:33 +0800

    bfs

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-parse_titles_v2
+from itertools import takewhile
+
+numbers = [2, 4, 5, 6, 7, 8, 9, 10]
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+even_numbers = takewhile(is_even, numbers)
+
+for num in even_numbers:
+    print(num)

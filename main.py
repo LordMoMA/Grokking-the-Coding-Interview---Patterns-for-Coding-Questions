870374f59fec0daf4cc9c586f64cb6c541e20d8e
David Lee <lihongyi0512@gmail.com>
2023-05-01 23:51:19 +0800

    bfs

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-# find if there are three numbers in three lists that add up to 12
-def find_twelve(num_list1, num_list2, num_list3):
-    for num1 in num_list1:
-        for num2 in num_list2:
-            for num3 in num_list3:
-                if num1 + num2 + num3 == 12:
-                    return num1, num2, num3
+s = '''--
+example
+--'''
+
+
+def strip_hyphen(s):
+    yield s.strip()
+
+
+print(strip_hyphen(s))

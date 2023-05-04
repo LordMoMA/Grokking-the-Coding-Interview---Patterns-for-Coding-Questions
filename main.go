b26bf4ddf0d5dbdd645d8b09cb04201d019c6a30
David Lee <lihongyi0512@gmail.com>
2023-05-04 16:02:10 +0800

    golang-code

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
-import "fmt"
- 
-func main() {
-   // Pointer to a string
-   username := "Go developer"
-   // Trying to change username by passing it a memory address using the & operator
-   changeNameByReference(&username)
-   // Print the new username value
-   fmt.Println(username)
- 
+import "log"
+
+type Animal interface {
+	// The method set of an interface type is its interface
+	Walk()
 }
- 
-func changeNameByReference(name *string) {
-   // name : Pointer to variable
-   // dereference name variable using the * operator
-   *name = "Go and cloud developer"
+
+type Cat struct {
+	name string
+}
+
+// A method of type T
+func (c Cat) Walk() {
+	log.Println("Animal name:", c.name)
+}
+
+// A method of type *T
+func (c *Cat) changeName(newName string) {
+	c.name = newName
+}
+
+func main() {
+	c1 := &Cat{name: "Tom"} // Pointer type
+	c2 := Cat{name: "Katie"}
+
+	// c1 with pointer receiver : Has all methods attached to both *Car(pointer) and Cat(Non-pointer)
+	c1.changeName("Tommy")
+	// C1 has access to Walk() method although Walk() method expect a pointer receiver
+	c1.Walk()
+	// c2 non-pointer receiver: // c2 with value receiver : Has all methods attached to Cat(Non-pointer)
+	c2.changeName("Kate")
+	c2.Walk()
 }

0c41573a60c2a41a730dc49aa5e72494fc3520f3
David Lee <lihongyi0512@gmail.com>
2023-05-13 01:42:42 +0800

    python-craftsman

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-# A Python program to generate squares from 1
-# to 100 using yield and therefore generator
+# 去过普吉岛的人员数据
+users_visited_phuket = [
+    {"first_name": "Sirena", "last_name": "Gross", "phone_number": "650-568-0388", "date_visited": "2018-03-14"},
+    {"first_name": "James", "last_name": "Ashcraft", "phone_number": "412-334-4380", "date_visited": "2014-09-16"},
+    ... ...
+]
 
-# An infinite generator function that prints
-# next square number. It starts with 1
+# 去过新西兰的人员数据
+users_visited_nz = [
+    {"first_name": "Justin", "last_name": "Malcom", "phone_number": "267-282-1964", "date_visited": "2011-03-13"},
+    {"first_name": "Albert", "last_name": "Potter", "phone_number": "702-249-3714", "date_visited": "2013-09-11"},
+    ... ...
+]
+class VisitRecord:
+    """旅游记录
+    """
 
-
-def nextSquare():
-    i = 1
-
-    # An Infinite loop to generate squares
-    while True:
-        yield i*i
-        i += 1  # Next execution resumes
-        # from this point
-
-
-# Driver code to test above generator
-# function
-for num in nextSquare():
-    if num > 100:
-        break
-    print(num)
+    def __init__(self, first_name, last_name, phone_number, date_visited):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.date_visited = date_visited

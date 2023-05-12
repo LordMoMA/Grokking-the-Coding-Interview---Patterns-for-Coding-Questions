81cdbfb17d49061427fec04f9d65b3d422e43123
David Lee <lihongyi0512@gmail.com>
2023-05-12 23:52:20 +0800

    python-craftsman

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-class Foo:
-    def __call__(self):
-        print("Hello, __call___")
+import time
+import functools
 
 
-foo = Foo()
+class DelayFunc:
+    def __init__(self,  duration, func):
+        self.duration = duration
+        self.func = func
 
-# OUTPUT: True
-print(callable(foo))
-# 调用 foo 实例
-# OUTPUT: Hello, __call__
-foo()
+    def __call__(self, *args, **kwargs):
+        print(f'Wait for {self.duration} seconds...')
+        time.sleep(self.duration)
+        return self.func(*args, **kwargs)
+
+    def eager_call(self, *args, **kwargs):
+        print('Call without delay')
+        return self.func(*args, **kwargs)
+
+
+def delay(duration):
+    """装饰器：推迟某个函数的执行。同时提供 .eager_call 方法立即执行
+    """
+    # 此处为了避免定义额外函数，直接使用 functools.partial 帮助构造
+    # DelayFunc 实例
+    return functools.partial(DelayFunc, duration)

800c69ff42b775f808db58c58da92ac658b33923
David Lee <lihongyi0512@gmail.com>
2023-05-13 00:38:46 +0800

    python-craftsman

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-import wrapt
+import time
 import random
 
 
-def provide_number(min_num, max_num):
-    @wrapt.decorator
-    def wrapper(wrapped, instance, args, kwargs):
-        # 参数含义：
-        #
-        # - wrapped：被装饰的函数或类方法
-        # - instance：
-        #   - 如果被装饰者为普通类方法，该值为类实例
-        #   - 如果被装饰者为 classmethod 类方法，该值为类
-        #   - 如果被装饰者为类/函数/静态方法，该值为 None
-        #
-        # - args：调用时的位置参数（注意没有 * 符号）
-        # - kwargs：调用时的关键字参数（注意没有 ** 符号）
-        #
-        num = random.randint(min_num, max_num)
-        # 无需关注 wrapped 是类方法或普通函数，直接在头部追加参数
-        args = (num,) + args
-        return wrapped(*args, **kwargs)
-    return wrapper
+def timer(wrapped):
+    """装饰器：记录并打印函数耗时"""
+    def decorated(*args, **kwargs):
+        st = time.time()
+        ret = wrapped(*args, **kwargs)
+        print('execution take: {} seconds'.format(time.time() - st))
+        return ret
+    return decorated
 
 
-class Foo:
-    @provide_number(1, 100)
-    def print_random_number(self, num):
-        print(num)
+@timer
+def random_sleep():
+    """随机睡眠一小会"""
+    time.sleep(random.random())
 
 
-# OUTPUT: 48
-Foo().print_random_number()
+print(random_sleep.__name__)
+# 输出 'decorated'
+print(random_sleep.__doc__)
+# 输出 None

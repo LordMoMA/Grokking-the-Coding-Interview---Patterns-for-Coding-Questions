9363e62b02c0c8f09874302a887845d825ab1a1d
David Lee <lihongyi0512@gmail.com>
2023-05-13 22:40:17 +0800

    python-craftsman

--- main2.py
+++ main2.py
@@ -1,16 +1,11 @@
-class CreateAccountError(Exception):
-    pass
+# importing "heapq" to implement heap queue
+import heapq
 
+# initializing list
+li = [5, 7, 9, 1, 3]
 
-def create_account(username, password):
-    if not username:
-        raise CreateAccountError("Username is required")
-    if not password:
-        raise CreateAccountError("Password is required")
-    # code to create account goes here
+# using heapify to convert list into heap
+heapq.heapify(li)
 
-
-try:
-    create_account("", "password123")
-except CreateAccountError as e:
-    print("An error occurred creating the account:", e)
+# printing created heap
+print("The created heap is : ", (list(li)))

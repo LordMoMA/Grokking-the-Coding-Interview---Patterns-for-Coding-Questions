a1675c6f987a430d6a0083165d026ab5129e8a65
David Lee <lihongyi0512@gmail.com>
2023-05-14 22:07:51 +0800

    python-craftsman

--- main2.py
+++ main2.py
@@ -7,18 +7,18 @@ li = [5, 7, 9, 1, 3]
 # using heapify to convert list into heap
 heapq.heapify(li)
 
-# printing created heap
-print("The created heap is : ", end="")
-print(list(li))
+# # printing created heap
+# print("The created heap is : ", end="")
+# print(list(li))
 
-# using heappush() to push elements into heap
-# pushes 4
-heapq.heappush(li, 4)
+# # using heappush() to push elements into heap
+# # pushes 4
+# heapq.heappush(li, 4)
 
-# printing modified heap
-print("The modified heap after push is : ", end="")
-print(list(li))
+# # printing modified heap
+# print("The modified heap after push is : ", end="")
+# print(list(li))
 
-# using heappop() to pop smallest element
-print("The popped and smallest element is : ", end="")
-print(heapq.heappop(li))
+# # using heappop() to pop smallest element
+# print("The popped and smallest element is : ", end="")
+# print(heapq.heappop(li))

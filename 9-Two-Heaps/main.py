a42b6f0133409e96c5eabcaba7bc35b113d5ade9
David Lee <lihongyi0512@gmail.com>
2023-05-14 22:51:12 +0800

    python-craftsman

--- 9-Two-Heaps/main.py
+++ 9-Two-Heaps/main.py
@@ -51,3 +51,29 @@ class MedianFinder:
             return (self.minHeap[0] - self.maxHeap[0]) / 2
         return self.minHeap[0]
 # time complexity: O(logn), space complexity: O(n)
+
+
+'''
+480. Sliding Window Median
+https://stackoverflow.com/questions/68510425/removing-value-from-heap-why-siftup-and-siftdown-need-to-be-called
+
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle values.
+
+For examples, if arr = [2,3,4], the median is 3.
+For examples, if arr = [1,2,3,4], the median is (2 + 3) / 2 = 2.5.
+You are given an integer array nums and an integer k. There is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the median array for each window in the original array. Answers within 10-5 of the actual value will be accepted.
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+Explanation: 
+Window position                Median
+---------------                -----
+[1  3  -1] -3  5  3  6  7        1
+ 1 [3  -1  -3] 5  3  6  7       -1
+ 1  3 [-1  -3  5] 3  6  7       -1
+ 1  3  -1 [-3  5  3] 6  7        3
+ 1  3  -1  -3 [5  3  6] 7        5
+ 1  3  -1  -3  5 [3  6  7]       6
+'''

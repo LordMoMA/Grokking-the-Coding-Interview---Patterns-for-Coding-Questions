c3a9af18f6a5318bd434808ab36de42c1ee56544
David Lee <lihongyi0512@gmail.com>
2023-05-15 00:18:16 +0800

    heapq

---
+++ main3.py
@@ -0,0 +1,12 @@
+# Python program to demonstrate
+# dictionary
+
+
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("Dictionary:")
+print(Dict)
+print(Dict[1])
+
+# Uncommenting this print(Dict[4])
+# will raise a KeyError as the
+# 4 is not present in the dictionary

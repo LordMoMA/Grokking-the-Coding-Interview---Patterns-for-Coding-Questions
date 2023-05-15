72ef8d07abc0a9a869d22992d636c7a9de3eaae7
David Lee <lihongyi0512@gmail.com>
2023-05-15 22:59:38 +0800

    heapq

--- main3.py
+++ main3.py
@@ -1,16 +1,25 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
 
-    def __format__(self, format_spec):
-        if format_spec == 'long':
-            return f'{self.name} is {self.age} years old.'
-        elif format_spec == 'simple':
-            return f'{self.name}({self.age})'
-        raise ValueError('invalid format spec')
+class Events:
+    def __init__(self, events):
+        self.events = events
+
+    def __len__(self):
+        """自定义长度，将会被用来做布尔判断"""
+        return len(self.events)
+
+    def __getitem__(self, index):
+        """自定义切片方法"""
+        # 直接将 slice 切片对象透传给 events 处理
+        return self.events[index]
 
 
-piglei = Student('piglei', '18')
-print('{0:simple}'.format(piglei))
-print('{0:long}'.format(piglei))
+events = Events([
+    'computer started',
+    'os launched',
+    'docker started',
+    'os stopped',
+])
+
+# 判断是否有内容，打印第二个和第三个对象
+if events:
+    print(events[1:3])

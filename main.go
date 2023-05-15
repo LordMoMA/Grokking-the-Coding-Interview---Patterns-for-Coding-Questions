28e62ce718c6b5c7a24b367573d377730f8def82
David Lee <lihongyi0512@gmail.com>
2023-05-16 00:29:08 +0800

    heapq

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import "strings"
+
+func main(){
+	// This works fine
+str := "abc"
+str = str + "def"
+
+// This is more efficient if you are combining lots of strings
+var sb strings.Builder
+sb.WriteString("abc")
+sb.WriteString("def")
+}

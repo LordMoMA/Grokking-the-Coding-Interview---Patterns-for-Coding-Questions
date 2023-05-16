f1c8593015951134f67947f78dd096aeaf2b1550
David Lee <lihongyi0512@gmail.com>
2023-05-17 02:20:36 +0800

    heapq

--- main3.py
+++ main3.py
@@ -1,14 +1,12 @@
 def find_potential_customers_v1():
-    """找到去过普吉岛但是没去过新西兰的人
-    """
-    for phuket_record in users_visited_phuket:
+    for hk_record in users_visited_hk:
         is_potential = True
-        for nz_record in users_visited_nz:
-            if phuket_record['first_name'] == nz_record['first_name'] and \
-                    phuket_record['last_name'] == nz_record['last_name'] and \
-                    phuket_record['phone_number'] == nz_record['phone_number']:
+        for nyc_record in users_visited_nyc:
+            if hk_record['first_name'] == nyc_record['first_name'] and \
+                    hk_record['last_name'] == nyc_record['last_name'] and \
+                    hk_record['phone_number'] == nyc_record['phone_number']:
                 is_potential = False
                 break
 
         if is_potential:
-            yield phuket_record
+            yield hk_record

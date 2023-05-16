d79f5531aad89fc061761fdf6ad73c95a464be14
David Lee <lihongyi0512@gmail.com>
2023-05-17 03:22:45 +0800

    heapq

--- main3.py
+++ main3.py
@@ -1,21 +1,86 @@
+import io
+import sys
+from typing import Generator
 
-class Events:
-    def __init__(self, events):
-        self.events = events
-
-    def __len__(self):
-        return len(self.events)
-
-    def __getitem__(self, index):
-        return self.events[index]
+import requests
+from lxml import etree
 
 
-events = Events([
-    'Game of Thrones begins',
-    'Avengers Endgame released',
-    'Python 3.8 released',
-    'COVID-19 declared a pandemic',
-])
+class Post:
+    """HN(https://news.ycombinator.com/) 上的条目
 
-if events:
-    print(events[1:3])
+    :param title: 标题
+    :param link: 链接
+    :param points: 当前得分
+    :param comments_cnt: 评论数
+    """
+
+    def __init__(self, title: str, link: str, points: str, comments_cnt: str):
+        self.title = title
+        self.link = link
+        self.points = int(points)
+        self.comments_cnt = int(comments_cnt)
+
+
+class HNTopPostsSpider:
+    """抓取 HackerNews Top 内容条目
+
+    :param fp: 存储抓取结果的目标文件对象
+    :param limit: 限制条目数，默认为 5
+    """
+    ITEMS_URL = 'https://news.ycombinator.com/'
+    FILE_TITLE = 'Top news on HN'
+
+    def __init__(self, fp: io.TextIOBase, limit: int = 5):
+        self.fp = fp
+        self.limit = limit
+
+    def fetch(self) -> Generator[Post, None, None]:
+        """从 HN 抓取 Top 内容
+        """
+        resp = requests.get(self.ITEMS_URL)
+
+        # 使用 XPath 可以方便的从页面解析出你需要的内容，以下均为页面解析代码
+        # 如果你对 xpath 不熟悉，可以忽略这些代码，直接跳到 yield Post() 部分
+        html = etree.HTML(resp.text)
+        items = html.xpath('//table[@class="itemlist"]/tr[@class="athing"]')
+        for item in items[:self.limit]:
+            node_title = item.xpath('./td[@class="title"]/a')[0]
+            node_detail = item.getnext()
+            points_text = node_detail.xpath('.//span[@class="score"]/text()')
+            comments_text = node_detail.xpath('.//td/a[last()]/text()')[0]
+
+            yield Post(
+                title=node_title.text,
+                link=node_title.get('href'),
+                # 条目可能会没有评分
+                points=points_text[0].split()[0] if points_text else '0',
+                comments_cnt=comments_text.split()[0]
+            )
+
+    def write_to_file(self):
+        """以纯文本格式将 Top 内容写入文件
+        """
+        self.fp.write(f'# {self.FILE_TITLE}\n\n')
+        # enumerate 接收第二个参数，表示从这个数开始计数（默认为 0）
+        for i, post in enumerate(self.fetch(), 1):
+            self.fp.write(f'> TOP {i}: {post.title}\n')
+            self.fp.write(f'> 分数：{post.points} 评论数：{post.comments_cnt}\n')
+            self.fp.write(f'> 地址：{post.link}\n')
+            self.fp.write('------\n')
+
+
+def main():
+
+    # with open('/tmp/hn_top5.txt') as fp:
+    #     crawler = HNTopPostsSpider(fp)
+    #     crawler.write_to_file()
+
+    # 因为 HNTopPostsSpider 接收任何 file-like 的对象，所以我们可以把 sys.stdout 传进去
+    # 实现往控制台标准输出打印的功能
+    crawler = HNTopPostsSpider(sys.stdout)
+    crawler.write_to_file()
+
+
+if __name__ == '__main__':
+    main()

b307673d575fe2b4219ffca528732b6feacd8094
David Lee <lihongyi0512@gmail.com>
2023-05-17 03:01:06 +0800

    heapq

--- main3.py
+++ main3.py
@@ -1,16 +1,21 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class Events:
+    def __init__(self, events):
+        self.events = events
 
-    def __format__(self, format_spec):
-        if format_spec == 'long':
-            return f'{self.name} is {self.age} years old.'
-        elif format_spec == 'simple':
-            return f'{self.name}({self.age})'
-        raise ValueError('invalid format spec')
+    def is_empty(self):
+        return not bool(self.events)
+
+    def list_events_by_range(self, start, end):
+        return self.events[start:end]
 
 
-david = Student('david', '33')
-print('{0:simple}'.format(david))
-print('{0:long}'.format(david))
+events = Events([
+    'computer started',
+    'os launched',
+    'docker started',
+    'os stopped',
+])
+
+# 判断是否有内容，打印第二个和第三个对象
+if not events.is_empty():
+    print(events.list_events_by_range(1, 3))

d0621f329d2575309949376c01fc92657df5f0d5
David Lee <lihongyi0512@gmail.com>
2023-05-17 03:05:35 +0800

    heapq

--- main3.py
+++ main3.py
@@ -1,14 +1,21 @@
-from collections import namedtuple
 
-Event = namedtuple('Event', ['title'])
+class Events:
+    def __init__(self, events):
+        self.events = events
 
-events = [
-    Event('Game of Thrones begins'),
-    Event('Avengers Endgame released'),
-    Event('Python 3.8 released'),
-    Event('COVID-19 declared a pandemic'),
-]
+    def __len__(self):
+        return len(self.events)
 
-# Check if there are any events and print the second and third objects
+    def __getitem__(self, index):
+        return self.events[index]
+
+
+events = Events([
+    'Game of Thrones begins',
+    'Avengers Endgame released',
+    'Python 3.8 released',
+    'COVID-19 declared a pandemic',
+])
+
 if events:
     print(events[1:3])

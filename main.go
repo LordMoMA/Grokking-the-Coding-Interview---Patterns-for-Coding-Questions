92884c7983542c6b77346f469f2de6b9adb9e669
David Lee <lihongyi0512@gmail.com>
2023-06-01 00:03:31 +0800

    heapq

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
-	original := "this is a string"
-	var b []byte
-	b = []byte(original)
-	fmt.Printf("byte: %s", string(b))
+	// original := "this is a string"
+	// var b []byte
+	// b = []byte(original)
+	// fmt.Printf("byte: %s", string(b))
+}
+
+type user struct {
+	id       int
+	username string
+	fullname string
+	email    string
+}
+
+func setup_test_user() *user {
+	sam := user{1001, "sammy", "Sam Winchester", "sam@winchester.org"}
+	return &sam
 }
 
 func Fib(n int) int {

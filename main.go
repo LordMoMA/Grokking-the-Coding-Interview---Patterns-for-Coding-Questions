a35532d6d51c74a33cf007687e850fff448bffe5
David Lee <lihongyi0512@gmail.com>
2023-05-31 23:14:48 +0800

    heapq

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	original := "this is a string"
 	var b []byte
-	b = []byte(original) 
-	fmt.Printf("byte: %s",string(b) )
+	b = []byte(original)
+	fmt.Printf("byte: %s", string(b))
 }
 
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
